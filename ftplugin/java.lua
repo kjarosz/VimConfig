e330c1c151b9de45116c9c0aa727775c31800cb8
Kamil Jarosz <kjarosz19946@gmail.com>
2024-05-18 08:31:38 -0400

    Remap some LSP shortcuts

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -26,16 +26,23 @@ local config = {
     root_dir = function() vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]) end,
 }
 
+local opts = {buffer = bufnr, remap = false}
+
+local function map(mode, keys, action, desc)
+  opts["desc"] = desc
+  vim.keymap.set(mode, keys, action, opts)
+end
+
 print("LSP Zero attached")
-vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-vim.keymap.set("n", "go", function() vim.lsp.buf.type_definition() end, opts)
-vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+map("n", "gd", function() vim.lsp.buf.definition() end, "[G]o to [D]efinition")
+map("n", "go", function() vim.lsp.buf.type_definition() end, "Show type definition")
+map("n", "K", function() vim.lsp.buf.hover() end, "Open hover menu")
+map("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, "[V]iew [W]orkspace [S]ymbol")
+map("n", "<leader>vd", function() vim.diagnostic.open_float() end, "[V]iew [D]iagnostic")
+map("n", "[d", function() vim.diagnostic.goto_next() end, "Next [d]iagnostic")
+map("n", "]d", function() vim.diagnostic.goto_prev() end, "Previous [d]iagnostic")
+map("n", "<leader>va", function() vim.lsp.buf.code_action() end, "[V]iew [C]ode [A]ctions")
+map("n", "<leader>vrr", function() vim.lsp.buf.references() end, "[V]iew [R]efe[r]ences")
+map("n", "<leader>rn", function() vim.lsp.buf.rename() end, "[R]e[n]ame")
+map("i", "<C-h>", function() vim.lsp.buf.signature_help() end, "Signature [H]elp")
 require('lspconfig').jdtls.setup(config)
